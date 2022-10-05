eb3ef19ac6bef0ebf1acb49710b43d7883c21cba
Pablo Causa <causapablo@gmail.com>
2022-10-05 09:20:33 -0300

    Correction of group

--- src/components/mobile/Mobile.jsx
+++ src/components/mobile/Mobile.jsx
@@ -10,7 +10,7 @@ import { GrSoundcloud } from 'react-icons/gr';
 const Mobile = () => {
     return (
         <section className='overflow-hidden mt-24'>
-            <div className='w-full group'>
+            <div className='w-full'>
 
                 <div className='md:hidden flex w-full group'>
                     <div className='flex group-hover:pause whitespace-nowrap animate-scrolling animation-delay-4 text-gray-300'>
@@ -26,21 +26,24 @@ const Mobile = () => {
                         <a href="https://soundcloud.com/german-brigante" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><GrSoundcloud className='mr-2' />SOUNDCLOUD<IoMdArrowDropright className='' /></button></a>
                     </div>
                 </div>
-                <div className='md:hidden flex w-full group mt-[275px]'>
-                    <div className='flex group-hover:pause whitespace-nowrap animate-scrolling animation-delay-4 text-gray-300'>
-                        <a href="https://www.beatport.com/artist/german-brigante/128913" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><SiBeatport className='mr-2' />BEATPORT<IoMdArrowDropright className=' ' /></button></a>
-                        <a href="https://music.apple.com/us/artist/german-brigante/362418980" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsApple className='mr-2' />APPLE MUSIC<IoMdArrowDropright className=' ' /></button></a>
-                        <a href="https://www.youtube.com/channel/UCMYdbWdVIKF6roLCLyjOU4Q/videos" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsYoutube className='mr-2' />YOUTUBE<IoMdArrowDropright className=' ' /></button></a>
-                        <a href="https://www.facebook.com/germanbrigantemusic" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><AiFillFacebook className='mr-2' />FACEBOOK<IoMdArrowDropright className=' ' /></button></a>
+                <div className='mt-[275px]'>
+                    <div className='md:hidden flex w-full group'>
+                        <div className='flex group-hover:pause whitespace-nowrap animate-scrolling animation-delay-4 text-gray-300'>
+                            <a href="https://www.beatport.com/artist/german-brigante/128913" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><SiBeatport className='mr-2' />BEATPORT<IoMdArrowDropright className=' ' /></button></a>
+                            <a href="https://music.apple.com/us/artist/german-brigante/362418980" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsApple className='mr-2' />APPLE MUSIC<IoMdArrowDropright className=' ' /></button></a>
+                            <a href="https://www.youtube.com/channel/UCMYdbWdVIKF6roLCLyjOU4Q/videos" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsYoutube className='mr-2' />YOUTUBE<IoMdArrowDropright className=' ' /></button></a>
+                            <a href="https://www.facebook.com/germanbrigantemusic" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><AiFillFacebook className='mr-2' />FACEBOOK<IoMdArrowDropright className=' ' /></button></a>
+                        </div>
+                        <div className='flex group-hover:pause whitespace-nowrap animate-scrolling2 animation-delay-2 text-gray-300'>
+                            <a href="https://www.beatport.com/artist/german-brigante/128913" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><SiBeatport className='mr-2' />BEATPORT<IoMdArrowDropright className=' ' /></button></a>
+                            <a href="https://music.apple.com/us/artist/german-brigante/362418980" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsApple className='mr-2' />APPLE MUSIC<IoMdArrowDropright className=' ' /></button></a>
+                            <a href="https://www.youtube.com/channel/UCMYdbWdVIKF6roLCLyjOU4Q/videos" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsYoutube className='mr-2' />YOUTUBE<IoMdArrowDropright className=' ' /></button></a>
+                            <a href="https://www.facebook.com/germanbrigantemusic" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><AiFillFacebook className='mr-2' />FACEBOOK<IoMdArrowDropright className=' ' /></button></a>
+                        </div>
                     </div>
-                    <div className='flex group-hover:pause whitespace-nowrap animate-scrolling2 animation-delay-2 text-gray-300'>
-                        <a href="https://www.beatport.com/artist/german-brigante/128913" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><SiBeatport className='mr-2' />BEATPORT<IoMdArrowDropright className=' ' /></button></a>
-                        <a href="https://music.apple.com/us/artist/german-brigante/362418980" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsApple className='mr-2' />APPLE MUSIC<IoMdArrowDropright className=' ' /></button></a>
-                        <a href="https://www.youtube.com/channel/UCMYdbWdVIKF6roLCLyjOU4Q/videos" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><BsYoutube className='mr-2' />YOUTUBE<IoMdArrowDropright className=' ' /></button></a>
-                        <a href="https://www.facebook.com/germanbrigantemusic" target="_blank" rel="noreferrer"><button className='w-full px-4 space-x-1 flex justify-between items-center font-extrabold md:bg-black bg-opacity-90 md:text-gray-300  hover:shadow-md hover:bg-[#C21010] hover:bg-opacity-10 hover:border-2 hover:border-[#C21010] hover:duration-300'><AiFillFacebook className='mr-2' />FACEBOOK<IoMdArrowDropright className=' ' /></button></a>
-                    </div>
+
                 </div>
-                
+
             </div>
         </section>
     );
