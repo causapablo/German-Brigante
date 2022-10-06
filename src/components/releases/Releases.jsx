3c5878cc9d063c0ba36b34b221da91f36149b7a1
Pablo Causa <causapablo@gmail.com>
2022-10-06 08:29:40 -0300

    Releases and youtube dimensions

--- src/components/releases/Releases.jsx
+++ src/components/releases/Releases.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { SiSemanticrelease } from 'react-icons/si'
 const Releases = () => {
     return (
-        <div className='container mx-auto flex flex-col space-y-3 px-10'>
-            <div className='text-gray-300 flex items-center text-4xl font-extrabold px-10'>
+        <div className='container mx-auto flex flex-col space-y-3 px-10 h-fit'>
+            <div className='text-gray-300 text-2xl flex items-center md:text-4xl font-extrabold px-5'>
                 <SiSemanticrelease />
                 <h1 className='ml-2'>Lastest Releases</h1>
             </div>
@@ -12,8 +12,8 @@ const Releases = () => {
             </div>
             <div className='grid gap-2 lg:grid lg:grid-cols-2 lg:gap-3'>
                 <a href="https://www.beatport.com/release/german-brigante-remixes/3785309" target='_blank' rel='noreferrer'>
-                    <div className='flex h-48 p-4 lg:text-xl font-bold bg-black bg-opacity-90 text-gray-300 hover:border-[#C21010] hover:border '>
-                        <img className='w-40' src="https://geo-media.beatport.com/image_size/1400x1400/68576b5b-78e9-48a5-aac3-5e9c268dd409.jpg" alt="new released" />
+                    <div className='md:flex h-fit p-4 lg:text-xl font-bold bg-black bg-opacity-90 text-gray-300 hover:border-[#C21010] hover:border '>
+                        <img className='md:w-40' src="https://geo-media.beatport.com/image_size/1400x1400/68576b5b-78e9-48a5-aac3-5e9c268dd409.jpg" alt="new released" />
                         <div className='pl-6 space-y-6 h-fit'>
                             <div>NOISSIER OCLUM HELIENA GERMAN BRIGANTE</div>
                             <div>GERMAN BRIGANTE REMIXES</div>
@@ -22,8 +22,8 @@ const Releases = () => {
                     </div>
                 </a>
                 <a href="https://www.beatport.com/release/magma/3856875" target='_blank' rel='noreferrer'>
-                    <div className='flex h-48 p-4 lg:text-xl font-bold bg-black bg-opacity-90 text-gray-300 hover:border-[#C21010] hover:border'>
-                        <img className='w-40' src="https://geo-media.beatport.com/image_size/1400x1400/da807b71-f706-4d83-a207-4a82eb1c94bd.jpg" alt="new released" />
+                    <div className='md:flex h-fit p-4 lg:text-xl font-bold bg-black bg-opacity-90 text-gray-300 hover:border-[#C21010] hover:border'>
+                        <img className='md:w-40' src="https://geo-media.beatport.com/image_size/1400x1400/da807b71-f706-4d83-a207-4a82eb1c94bd.jpg" alt="new released" />
                         <div className='pl-6 space-y-6'>
                             <div>GERMAN BRIGANTE YAMIL</div>
                             <div>MAGMA</div>
