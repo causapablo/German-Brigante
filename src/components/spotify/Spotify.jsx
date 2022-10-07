2c0eafab243215de15540eb72540277aa7ab6a60
Pablo Causa <causapablo@gmail.com>
2022-10-07 14:09:53 -0300

    spotify corrected

--- src/components/spotify/Spotify.jsx
+++ src/components/spotify/Spotify.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BsSpotify } from 'react-icons/bs';
 const Spotify = () => {
     return (
-        <div className='container md:mx-auto mx-4 py-20'>
+        <div className='container md:mx-auto py-20'>
             <div className='text-gray-300 flex items-center text-4xl font-extrabold px-10'>
                 <BsSpotify />
                 <h1 className='ml-2'>Spotify</h1>
@@ -10,7 +10,7 @@ const Spotify = () => {
             <div className='px-5 py-5 md:px-0'>
                 <div className='container bg-white mx-auto h-[1px]'></div>
             </div>
-            <iframe style={{ 'border-radius': '12px' }} title='German Brigante' src="https://open.spotify.com/embed/artist/2h7fN6KOZVgWQ39sYK0jJm?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe className='px-2' style={{ 'border-radius': '12px' }} title='German Brigante' src="https://open.spotify.com/embed/artist/2h7fN6KOZVgWQ39sYK0jJm?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
     );
 };
