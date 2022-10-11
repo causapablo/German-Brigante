14f1bca0358cb80955fbae46e50d40b1ecaf16fb
Pablo Causa <causapablo@gmail.com>
2022-10-11 06:40:44 -0300

    Manitox Label

---
+++ src/components/manitox/Manitox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Manitox = () => {
+    return (
+        <div className='container mx-auto py-10 h-fit'>
+            <div className='text-gray-300 flex items-center text-4xl font-extrabold px-10'>
+                <AiOutlineYoutube />
+                <h1 className='ml-2'>Manitox Record Label</h1>
+            </div>
+            <div className='px-5 py-5 md:px-0'>
+                <div className='container bg-white mx-auto h-[1px]'></div>
+            </div>
+            <div className='grid grid-cols-1 justify-items-center lg:grid-cols-2 gap-3'>
+                
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Manitox;
